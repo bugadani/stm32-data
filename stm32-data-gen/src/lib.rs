47156df63e965dfb4aeb990cc3d6d884398984b5
Dániel Buga <bugadani@gmail.com>
2024-06-09 07:41:45 +0800

    Allow using stm32-data-gen as a library

---
+++ stm32-data-gen/src/lib.rs
@@ -0,0 +1,23 @@
+pub mod check;
+pub mod chips;
+pub mod dma;
+pub mod docs;
+pub mod gpio_af;
+pub mod header;
+pub mod interrupts;
+pub mod memory;
+pub mod normalize_peris;
+pub mod perimap;
+pub mod rcc;
+pub mod registers;
+pub mod util;
+
+#[macro_export]
+macro_rules! regex {
+    ($re:literal) => {{
+        ::ref_thread_local::ref_thread_local! {
+            static managed REGEX: ::regex::Regex = ::regex::Regex::new($re).unwrap();
+        }
+        <REGEX as ::ref_thread_local::RefThreadLocal<::regex::Regex>>::borrow(&REGEX)
+    }};
+}
